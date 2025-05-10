8d3727e304bd0f21dc4d0fabeed3d6ce49c2f40e
Jona <llamasjonat@gmail.com>
2025-05-10 18:40:35 -0300

    fix: integrate prop on Header for back to home

--- app/event/[id]/manage/page.tsx
+++ app/event/[id]/manage/page.tsx
@@ -5,7 +5,7 @@ import { EventManagement } from '@/components/manage/event-management';
 export default function EventManagePage({ params }: { params: { id: string } }) {
   return (
     <>
-      <Header />
+      <Header backGoHome={true} />
       <main className='py-6 pb-20 md:pb-6'>
         <EventManagement eventId={params.id} />
       </main>
