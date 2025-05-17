9c1d00e75b46012d06378ee65f0ce84f041cacec
Jona <llamasjonat@gmail.com>
2025-05-17 15:27:23 -0300

    fix(layout): integrate LoginForm on AdminLayout

--- components/auth/admin-layout.tsx
+++ components/auth/admin-layout.tsx
@@ -1,21 +1,26 @@
-"use client"
+'use client';
 import { LoaderView } from '@/components/loader-view';
 import { useAuth } from '@/lib/auth-provider';
 import { useDraftEventContext } from '@/lib/draft-event-context';
+import { LoginForm } from '../login-form';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-    const { user, isAuthenticated } = useAuth();
-    const { draftEvent } = useDraftEventContext();
+  const { user, isAuthenticated } = useAuth();
+  const { draftEvent } = useDraftEventContext();
 
-    const isAllowed =
-        user?.loaded &&
-        isAuthenticated &&
-        (!draftEvent ||
-        (draftEvent?.id &&
-        (draftEvent.creatorId === user.id ||
-            draftEvent.team?.some((member) => member.userId === user.id))));
+  const isAllowed =
+    user?.loaded &&
+    isAuthenticated &&
+    (!draftEvent ||
+      (draftEvent?.id &&
+        (draftEvent.creatorId === user.id || draftEvent.team?.some((member) => member.userId === user.id))));
 
-    if (!isAllowed) return <LoaderView />;
+  if (!isAllowed)
+    return (
+      <div className='min-h-screen flex flex-col flex-1 items-center justify-center w-full max-w-md mx-auto py-6'>
+        <LoginForm />
+      </div>
+    );
 
-    return <>{children}</>;
-}   
+  return <>{children}</>;
+}
